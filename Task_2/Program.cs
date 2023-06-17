8de88d7834ddb19618dfb8f8ae9c5c1aa738adfb
Elena <elena_nn@list.ru>
2023-06-17 21:09:26 +0300

    Task_2

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -15,5 +15,5 @@ int By = Convert.ToInt32(Console.ReadLine()!);
 Console.Write("Введите координату по z точки B: ");
 int Bz = Convert.ToInt32(Console.ReadLine()!);
 
-double AB = Math.Sqrt((Math.Pow((Bx-Ax),2))+(Math.Pow((By-Ay),2))+(Math.Pow((Bz- Az),2)));
+double AB = Math.Sqrt((Math.Pow((Bx - Ax), 2)) + (Math.Pow((By - Ay), 2)) + (Math.Pow((Bz - Az), 2)));
 Console.WriteLine($"Расстояние между точками А и В в 3D пространстве, составляет {AB:f2}");
